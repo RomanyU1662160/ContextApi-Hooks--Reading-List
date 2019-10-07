ba7d4a8f78a90049a154874d837330daad9b692e
Romany Sefen <u1662160@unimail.hud.ac.uk>
2019-10-07 23:40:15 +0100

    Added the radius

--- src/components/styled/Button.jsx
+++ src/components/styled/Button.jsx
@@ -3,4 +3,5 @@ import styled from "styled-components";
 export const Button = styled.button`
   background: ${props => (props.primary ? "palevioletred" : "White")};
   color: ${props => (props.primary ? "white" : "palevioletred")};
+  border-radius: 3px; 
 `;
